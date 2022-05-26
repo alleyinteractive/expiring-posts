2cf3fe6c4a17195ba7ad9439baa28d0697ca057c
Sean Fisher <srtfisher@gmail.com>
2022-05-25 21:59:35 -0400

    Switching to mantle-framework/testkit

--- tests/test-cron.php
+++ tests/test-cron.php
@@ -2,9 +2,9 @@
 namespace Expiring_Posts\Tests;
 
 use Expiring_Posts\Expiring_Posts;
-use Mantle\Testing\Framework_Test_Case;
+use Mantle\Testkit\Test_Case;
 
-class Test_Cron extends Framework_Test_Case {
+class Test_Cron extends Test_Case {
 	public function test_expiration_check_scheduled() {
 		Expiring_Posts::instance();
 
