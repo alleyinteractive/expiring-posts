8c707f7d365d577efdbea766707b1600037d07a3
srtfisher <srtfisher@users.noreply.github.com>
2025-09-04 13:40:09 +0000

    Built changes for v0.3.0 from develop 72eacb14a80db8e2e3bf138c32473065301c00ee

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/expiring-posts',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '72eacb14a80db8e2e3bf138c32473065301c00ee',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/expiring-posts' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '72eacb14a80db8e2e3bf138c32473065301c00ee',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
